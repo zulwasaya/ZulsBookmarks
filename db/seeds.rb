864ee2e6fd2612a30fd753ab2a80a8ee79ae8d51
Zul wasaya <zul.wasaya@gmail.com>
2013-01-08 16:19:14 +0000

    seeded with 1 bookmark for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+   bookmarks= Bookmark.create([{search: 'ruby on rails'},{name: 'Ruby on Rails'},{url: 'rubyonrails.org'}])
