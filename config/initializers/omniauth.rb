35b2505b41ca89fc25f4613a577a07daea0b4f76
Zul Wasaya <zul.wasaya@gmail.com>
2013-04-20 07:54:26 +0100

    updated all keys and secrets for authorizations

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   # ==> Twitter
- provider :twitter, 'nLpcUc7W1Meknqy8ZCHBZw', 'wP0XAGiNypExc4ObpyCTOmP9rLFZyRT6U3FxyzRydA'
+ provider :twitter, 'yGYlXY7XVywwqGZS8hKMRg', 'XRsmr6UwDJr0Dfr82jdukBSTAFgGwF8r4b6dPUx5pq8'
 
   # ==> Facebook
- provider :facebook, '124983381024311', 'e57a6104170398577ca286e257fdef74'
+ provider :facebook, '143341512513758', 'f966cbdee6509c62d7c9b9b6cb3e0caa'
 
   # ==> GitHub
  provider :github, '11c75b100e99f0118ef4', '7fa145f31d689318596411b775457427b99cdb32'
 
   # ==> LinkedIn
- provider :linked_in, 'n9df58a1hsud', 'EiTKeQysAcA1sGLF'
+ provider :linked_in, '989bm69evo4m', 'mOotnZRXsskFcvKY'
 
 
 end
